95f6cb74f28c5e9e85b3cc3bf5c3689c3aeb1651
heritiertuyisenge520-source <heritiertuyisenge520@gmail.com>
2026-03-27 13:29:28 +0200

    packages

---
+++ src/screens/LiveBusTrackerScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const LiveBusTrackerScreen = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">Live Bus Tracker</Text>
+    </View>
+  );
+};
+
+export default LiveBusTrackerScreen;
