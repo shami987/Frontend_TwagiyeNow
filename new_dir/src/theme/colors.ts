4c9d56254f9326234e55afb5c98a66c7318f28b5
heritiertuyisenge520-source <heritiertuyisenge520@gmail.com>
2026-03-27 10:10:31 +0200

    folder

---
+++ new_dir/src/theme/colors.ts
@@ -0,0 +1,10 @@
+const colors = {
+  primary: '#1E8449',
+  primaryLight: '#E8F5E9',
+  white: '#FFFFFF',
+  black: '#000000',
+  gray: '#888888',
+  lightGray: '#F5F5F5',
+};
+
+export default colors;
